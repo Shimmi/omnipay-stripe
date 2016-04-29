68dfadba1cea6400bb7cfe40e3a7791545d5f340
Jiří Šimeček <jirka@simecek.org>
2016-04-29 12:07:19 +0200

    #1 Fix sending of credit card data

--- src/Message/AbstractRequest.php
+++ src/Message/AbstractRequest.php
@@ -197,7 +197,6 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest
         $data['address_zip'] = $card->getPostcode();
         $data['address_state'] = $card->getState();
         $data['address_country'] = $card->getCountry();
-        $data['email']           = $card->getEmail();
 
         return $data;
     }
